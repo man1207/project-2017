c9b3c27ddb1459b0b5911ebd4a4e6a3325822d43
Yaroslav Napadailo <the_m-a-n@mail.ru>
2017-02-20 16:46:24 +0600

    Add auth-form

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,53 @@
-/* Переписать файл согласно BEM-методологии */
+/* Пока без продакшна */
+
 var gulp = require('gulp'),
 	less = require('gulp-less'),
 	browserSync = require('browser-sync'),
 	concat = require('gulp-concat'),
-	uglify = require('gulp-uglifyjs'),
-	cssnano = require('gulp-cssnano'),
-	rename = require('gulp-rename'),
-	del = require('del');
+	rename = require('gulp-rename');
 
-gulp.task('less', function(){
-	return gulp.src('app/less/**/*.less')
+gulp.task('less', function() { //работа препроцессора
+	return gulp.src([
+		'src/less/*.less',
+		'src/less/global/*.less',
+		'src/blocks/**/*.less'
+		])
+		.pipe(rename({dirname: ''}))
+		.pipe(concat('style.less'))
 		.pipe(less())
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('src/css'))
 		.pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('browser-sync', function(){
+gulp.task('img', function(){
+	//return gulp.src([
+	//	'src/blocks/**/*.png',
+	//	'src/blocks/**/*.jpg'
+	//	])
+	//	.pipe(rename({dirname: ''}))
+	//	.pipe(gulp.dest('src/img'));
+	
+});
+
+gulp.task('browser-sync', function() {//запуск сервера
 	browserSync({
 		server: {
-			baseDir: 'app'
+			baseDir: 'src'
 		},
 		notify: false
 	});
-});
-
-gulp.task('scripts', function() {
-	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'])
-		.pipe(concat('libs.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('app/js'));
-});
-
-gulp.task('css-libs', function(){
-	return gulp.src(['app/libs/magnific-popup/dist/magnific-popup.css'])
-		.pipe(cssnano())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('app/css'));
-}); 
-
-gulp.task('watch', ['browser-sync', 'css-libs', 'less', 'scripts'], function(){
-	//Интересно как это работает. Проверить!
-	gulp.watch('app/less/**/*.less', ['less']);
-	gulp.watch('app/*.html', browserSync.reload);
-	gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-gulp.task('clean' , function() {
-	return del.sync('dist');
-});
-
-gulp.task('build', ['clean', 'less', 'scripts'], function(){
-	var buildCss = gulp.src([
-		'app/css/main.css',
-		'app/css/magnific-popup.min.css'])
-	.pipe(gulp.dest('dist/css'));
 	
-	var buildFonts = gulp.src('app/fonts/**/*')
-	.pipe(gulp.dest('dist/fonts'));
-	
-	
-	var buildJs = gulp.src('app/js/**/*')
-	.pipe(gulp.dest('dist/js'));
-	
-	var buildHtml = gulp.src('app/*.html')
-	.pipe(gulp.dest('dist'));
 });
 
+gulp.task('watch', ['browser-sync', 'img', 'less'], function() { //наблюдение за изменениями в коде
+	gulp.watch([
+		'src/less/*.less',
+		'src/less/global/*.less',
+		'src/blocks/**/*.less'
+		], ['less']);	
+	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch([
+		'src/blocks/**/*.jpg',
+		'src/blocks/**/*.png'
+		], ['img', browserSync.reload]);
+});
